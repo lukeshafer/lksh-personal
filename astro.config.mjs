440b15e77556f33d4a8b9a0775188cb4df75e9a4
Luke <hello@lukeshafer.com>
2023-05-28 22:28:08 -0400

    Update layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import { ecsstatic } from "@acab/ecsstatic/vite";
+import { defineConfig } from 'astro/config';
+import { ecsstatic } from '@acab/ecsstatic/vite';
+import solidJs from '@astrojs/solid-js';
+
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solidJs()],
+	integrations: [solidJs(), alpinejs()],
 	vite: {
 		plugins: [ecsstatic()],
 	},
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
 });
---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import("@types/prettier").Config} */
+module.exports = {
+	pluginSearchDirs: false,
+	useTabs: true,
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+	singleQuote: true,
+	trailingComma: 'es5',
+	tabWidth: 4,
+	useTabs: true,
+	printWidth: 100,
+	bracketSameLine: true,
+};
